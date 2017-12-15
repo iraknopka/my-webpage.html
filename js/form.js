20f1f44b23eadd944ea486c1e4d73fd3da63eb89
Iryna Korman <iraknopka7@gmail.com>
2017-12-15 18:13:40 +0000

    added form js

---
+++ js/form.js
@@ -0,0 +1,21 @@
+$(function(){
+   $('#contact-form input[type=submit]').click(sendForm); 
+});
+
+function sendForm(ev) {
+    const form  = document.getElementsByTagName('form')[0];
+    if (form.checkValidity()) {
+        ev.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/iraknopka7@gmail.com", 
+            method: "POST",
+            data: {
+                email: $('#email').val(),
+                message: $('#message').val()
+            },
+            dataType: "json"
+        })
+         .done( () => $('#thank-dialog').attr('open', 'open') )
+         .fail( () => $('#error-dialog').attr('open', 'open') );    
+    }
+}
